f9930a312ba1f4f1da52def8743eccd1e2f96fbd
VaishnaviGanagi <vaishnavivaishu8484@gmail.com>
2025-12-12 20:33:32 +0530

    create math1.py

---
+++ math1.py
@@ -0,0 +1,8 @@
+try:
+  a=float(input("first number:"))
+  b=float(input("second number:"))
+  print(f"add:{a+b}")
+  print(f"sub:{a-b}")
+except ValueError:
+  print("enter valid input")
+                
